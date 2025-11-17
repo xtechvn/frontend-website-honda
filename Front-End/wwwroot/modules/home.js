d6c23835dc3035d5a2e67f068eee77e7e3ee8bae
Hiếu <anhhieuk50@gmail.com>
2025-11-17 14:06:29 +0700

    update

--- Front-End/wwwroot/modules/home.js
+++ Front-End/wwwroot/modules/home.js
@@ -3,13 +3,22 @@
         var name = $('#name').val();
         var phone = $('#phone').val();
         var type = $('#type').val();
-        var selectedRadio = $('input[name="radio-401"]:checked').val(); 
+        if (name == '' || name == undefined) {
+            $('.name-error').show();
+            return false
+        }
+        if (phone == '' || phone == undefined) {
+            $('.phone-error').show();
+            return false
+        }
+
+        var selectedRadio = $('input[name="radio-401"]:checked').val();
         $.ajax({
             url: "/Home/Dangky",
             type: "post",
             data: { name: name, phone: phone, type: type, selectedRadio: selectedRadio },
             success: function (result) {
-               
+
                 if (result.status == 1) {
                     $('.ss-dk-dl').html(result.msg)
                     $('.ss-dk-dl').attr('style', 'color:red;display:block')
@@ -31,4 +40,32 @@
             }
         });
     });
+   
 });
+var _home = {
+    showname: function () {
+        var name = $('#name').val();
+        if (name == '' || name == undefined) {
+            $('.name-error').show();
+            
+        } else {
+            $('.name-error').hide();
+        }
+        
+    },
+    showphone: function () {
+
+        var phone = $('#phone').val();
+        const phoneRegex = /^(0[3|5|7|8|9])[0-9]{8}$/;
+        if (!phoneRegex.test(phone.value.trim())) {
+            $('.phone-error').html("Số điện thoại không đúng định dạng.");         
+        }
+        if (phone == '' || phone == undefined) {
+            $('.phone-error').html("Số điện thoại không được bỏ trống.");
+            $('.phone-error').show();
+            
+        } else {
+            $('.phone-error').hide();
+        }
+    }
+}
